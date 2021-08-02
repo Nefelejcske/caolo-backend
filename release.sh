b150f3afe17aaa8f3b007184c4894299d561e9eb
Daniel Kiss <littlesnorrboy@gmail.com>
2021-08-02 12:05:13 +0200

    Add retry logic to release.sh

--- release.sh
+++ release.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-set -e
+echo Release command starting
 
-
-echo "Release command starting"
+backoff=1
 
 /caolo/diesel migration run
 
-echo "Release command finished"
+while [ $? -ne 0 ]; do
+    if [ $backoff -gt 16 ]; then 
+        echo Release command failed repeatedly, aborting
+        exit 1
+    fi;
+
+    echo Release command failed, retrying in $backoff seconds
+    sleep $backoff;
+    backoff=$(($backoff * 2))
+
+    /caolo/diesel migration run
+done;
+
+echo Release command finished
