9f9be4361d04b95cf4a922bac1e227edaeb14ec7
Daniel Kiss <littlesnorrboy@gmail.com>
2021-10-11 18:20:48 +0200

    Serialize world iterators instead of collected vectors

--- sim/simulation/src/world/world_serde.rs
+++ sim/simulation/src/world/world_serde.rs
@@ -1,6 +1,43 @@
-use serde::{ser::SerializeStruct, Serialize, Serializer};
+use std::cell::RefCell;
+
+use serde::{
+    ser::{SerializeSeq, SerializeStruct},
+    Serialize, Serializer,
+};
 
 use crate::prelude::*;
+
+/// serialize only once, will consume the iterator
+struct SerializeIter<T: Serialize, It: Iterator<Item = T>> {
+    it: RefCell<It>,
+    _m: std::marker::PhantomData<T>,
+}
+
+impl<T: Serialize, It: Iterator<Item = T>> SerializeIter<T, It> {
+    fn new(it: It) -> Self {
+        Self {
+            it: RefCell::new(it),
+            _m: Default::default(),
+        }
+    }
+}
+
+impl<T: Serialize, It: Iterator<Item = T>> Serialize for SerializeIter<T, It> {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        let mut it = self.it.borrow_mut();
+
+        let mut state = serializer.serialize_seq(it.size_hint().1)?;
+
+        for item in &mut *it {
+            state.serialize_element(&item)?;
+        }
+
+        state.end()
+    }
+}
 
 impl Serialize for World {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
@@ -10,21 +47,21 @@ impl Serialize for World {
         let mut state = serializer.serialize_struct("CaoloWorld", 9)?;
         state.serialize_field("time", &self.time())?;
         // ser entities
-        state.serialize_field("bots", &self.entities.iterby_bot().collect::<Vec<_>>())?;
+        state.serialize_field("bots", &SerializeIter::new(self.entities.iterby_bot()))?;
         state.serialize_field(
             "structures",
-            &self.entities.iterby_structure().collect::<Vec<_>>(),
+            &SerializeIter::new(self.entities.iterby_structure()),
         )?;
         state.serialize_field(
             "resources",
-            &self.entities.iterby_resource().collect::<Vec<_>>(),
+            &SerializeIter::new(self.entities.iterby_resource()),
         )?;
         // ser users
-        state.serialize_field("users", &self.user.iterby_user().collect::<Vec<_>>())?;
+        state.serialize_field("users", &SerializeIter::new(self.user.iterby_user()))?;
         // ser scripts
-        state.serialize_field("scripts", &self.scripts.iterby_cao_ir().collect::<Vec<_>>())?;
+        state.serialize_field("scripts", &SerializeIter::new(self.scripts.iterby_cao_ir()))?;
         // ser rooms
-        state.serialize_field("rooms", &self.room.iterby_rooms().collect::<Vec<_>>())?;
+        state.serialize_field("rooms", &SerializeIter::new(self.room.iterby_rooms()))?;
         // ser config
         state.serialize_field(
             "room_properties",
