f709c1bedfc97037732352eb9a141dad27570e82
Daniel Kiss <littlesnorrboy@gmail.com>
2021-10-24 22:36:32 +0200

    Remove cao-profile dep

--- sim/simulation/src/utils.rs
+++ sim/simulation/src/utils.rs
@@ -1,11 +1,8 @@
 /// If `profile` feature is enabled, records high-level profiling information to `profile.csv`.
-/// Recording is done via a thread-local buffer and dedicated file writing thread, in an attempt to
-/// mitigate overhead.
 ///
 #[macro_export(local_inner_macros)]
 macro_rules! profile {
-    ($name: expr) => {
-        #[cfg(feature = "profile")]
-        cao_profile::profile!($name)
+    ($name: expr) => { 
+        // TODO
     };
 }
